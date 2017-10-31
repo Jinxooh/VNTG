f49d06f766dd9d2121a71b029be215a83f4a3e40
황진수-PC <cjswp122@gmail.com>
2017-10-31 17:04:36 +0900

    mongodb 세팅 연결 연습

--- vntg_backend/app/api/auth/index.js
+++ vntg_backend/app/api/auth/index.js
@@ -3,6 +3,8 @@ const Router = require('koa-router');
 const router = new Router();
 
 router.post('/login/:provider', (ctx, next) => {
+  console.log(ctx.request.body);
+  console.log(ctx.request.body.something);
   ctx.body = {
     provider: ctx.params.provider
   };
--- vntg_backend/app/index.js
+++ vntg_backend/app/index.js
@@ -1,16 +1,49 @@
 /* 모듈 불러오기 */
 const Koa = require('koa');
-const Router = require('koa-router');
-const bodyParser = require('koa-bodyparser');
-
-const api = require('./api');
+const serve = require('koa-static');
+const path = require('path');
 const app = new Koa();
+const mongo = require('koa-mongo');
+const mongoose = require('mongoose');
 
-// body-parser 적용
-app.use(bodyParser());
+const db = mongoose.createConnection('mongodb://localhost/vntg_db');
+console.log(db);
+// app.use(serve(path.resolve(__dirname, '../build')));
+// app.use(mongo());
+// app.use(mongo({
+//   host: 'localhost',
+//   port: 27017,
+//   db: 'vntg_db',
+//   msc: 100,
+//   min: 1
+// }));
+app.use(async (ctx, next) => {
+  ctx.body = 'hlllllll';
+})
 
-const router = new Router();
-router.use('/api', api.routes());
-app.use(router.routes());
 
-app.listen(3000);
+// const Router = require('koa-router');
+// const bodyParser = require('koa-bodyparser');
+// const mongoose = require('mongoose');
+// const api = require('./api');
+// const app = new Koa();
+
+// // body-parser 적용
+// app.use(bodyParser());
+
+// const router = new Router();
+// // router.use('/api', api.routes());
+// app.use(router.routes());
+
+// const db = mongoose.connection;
+// // db.on('error', console.error);
+// // db.once('openUri', () => {
+// //   console.log('Connected to mongod server');
+// // });
+
+// // const MONGODB_SEVER = 'mongodb://localhost/vntg_db'
+// // mongoose.createConnection(MONGODB_SEVER);
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
